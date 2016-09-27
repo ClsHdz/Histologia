6c182a6772151b0f60ed32ee0319bfdc7dfccc59
xp <xp@Android029>
2016-09-27 16:58:06 +0200

    Mejorada la base de datos y añadido las imagenes.

---
+++ juego2.php
@@ -0,0 +1,17 @@
+
+<style>
+    .float{
+        float: left;
+    }
+</style>
+<html>
+    <div class="container float" length="49%">
+        <button class="btn btn-block">Imagen</button><br>
+    </div>
+    <div class="container">
+        <button class="btn btn-block">AA</button><br>
+        <button class="btn btn-block">BB</button><br>
+        <button class="btn btn-block">CC</button><br>
+    </div>
+</html>
+
